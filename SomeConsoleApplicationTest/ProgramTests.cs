c516fbabd18b5ce908249ca02e389a913c05d7aa
PriscillaJB19 <ana.jimenez@grupobabel.com>
2022-12-27 11:59:29 -0600

    Proyecto

---
+++ SomeConsoleApplicationTest/ProgramTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using SomeConsoleApplication;
+using System.IO;
+using System;
+
+namespace SomeConsoleApplicationTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var actual = Program.AlwaysReturnsTrue();
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void Passthrough_ReturnsTheGivenValue()
+        {
+            // Arrange
+            var value = 42;
+
+            // Act
+            var actual = Program.Passthrough(value);
+
+            // Assert
+            Assert.AreEqual(actual, value);
+        }
+
+    }
+}
